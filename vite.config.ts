f1e699e78e3a1aea863f5eea74f66ef143fa494e
yerinra <yerinaleph@gmail.com>
2024-01-25 21:45:22 +0900

    🐛 Fix: proxy 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  server: {
+    proxy: {
+      "/api": {
+        // target: "https://openapi.naver.com/v1/search/book.json",
+        target: "http://www.aladin.co.kr/ttb/api/ItemSearch.aspx",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        secure: false,
+        ws: true,
+      },
+    },
+  },
+});
