5272397a0682d76e36b7b356764a2fe5f99e1345
yerinra <yerinaleph@gmail.com>
2024-01-25 23:05:59 +0900

    🎨 Fix: route와 outlet 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+import BasicLayout from "./components/BasicLayout";
+import LogInPage from "./pages/LogInPage";
+import InfoPage from "./pages/InfoPage";
+import ErrorPage from "./pages/ErrorPage";
+import SignUpPage from "./pages/SignUpPage";
+
+import BookShelfPage from "./pages/BookShelfPage";
+
+import { useRecoilState } from "recoil";
+import { loginState, userState } from "../src/store/userState";
+import { useEffect } from "react";
+import { auth } from "./service/firebase";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [login, setLogin] = useRecoilState(loginState);
+  const [user, setUser] = useRecoilState(userState);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setLogin(true);
+        setUser(user?.uid);
+      } else {
+        setLogin(false);
+      }
+      console.log("appp", user?.uid);
+    });
+  }, [login, user]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LogInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route element={<BasicLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/info" element={<InfoPage />} />
+          <Route path="/bookshelf" element={<BookShelfPage />} />
+        </Route>
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/BasicLayout.tsx
@@ -0,0 +1,16 @@
+import Footer from "./layout/Footer";
+import NavBar from "./layout/Navbar";
+import { Outlet } from "react-router-dom";
+const BasicLayout = () => {
+  return (
+    <>
+      <NavBar />
+      <section>
+        <Outlet />
+      </section>
+      <Footer />
+    </>
+  );
+};
+
+export default BasicLayout;
