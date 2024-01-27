0469c8781de1f389381e078f9fe2a0b53042b804
yerinra <yerinaleph@gmail.com>
2024-01-27 23:19:51 +0900

    ✨ Feat: RoutesPage 생성하여 App에서 Routes 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./App.css";
 
-import BasicLayout from "./components/BasicLayout";
-import LogInPage from "./pages/LogInPage";
-import InfoPage from "./pages/InfoPage";
-import ErrorPage from "./pages/ErrorPage";
-import SignUpPage from "./pages/SignUpPage";
-import BookDetailPage from "./pages/BookDetailPage";
-import BookShelfPage from "./pages/BookShelfPage";
+import RoutesPage from "./RoutesPage";
 
 import { useRecoilState } from "recoil";
 import { loginState, userState } from "../src/store/userState";
 import { useEffect } from "react";
 import { auth } from "./service/firebase";
-import HomePage from "./pages/HomePage";
-import BookSearchResultPage from "./pages/BookSearchResultPage";
 
 function App() {
   const [login, setLogin] = useRecoilState(loginState);
@@ -38,18 +30,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Router>
-        <Routes>
-          <Route path="/login" element={<LogInPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route element={<BasicLayout />}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/info" element={<InfoPage />} />
-            <Route path="/bookshelf" element={<BookShelfPage />} />
-            <Route path="/books/:keyword" element={<BookSearchResultPage />} />
-            <Route path="/book/:isbn" element={<BookDetailPage />} />
-          </Route>
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
+        <RoutesPage />
       </Router>
       <ReactQueryDevtools />
     </QueryClientProvider>
