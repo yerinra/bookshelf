503280a587e8fb534a966fe5d228aa2040127c9e
yerinra <yerinaleph@gmail.com>
2024-01-29 20:56:55 +0900

    ✨ Feat: ErrorPage 구현

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,11 @@
+const ErrorPage = () => {
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p className="mt-10">Sorry, an unexpected error has occurred.</p>
+      <p></p>
+    </div>
+  );
+};
+
+export default ErrorPage;
