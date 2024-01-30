51274ff22e1e1915b384ac3ab4917476ddf8cd38
yerinra <yerinaleph@gmail.com>
2024-01-31 00:27:50 +0900

    🎨  Update: minor updates(change user to currentUse)

--- src/pages/LogInPage.tsx
+++ src/pages/LogInPage.tsx
@@ -11,11 +11,11 @@ const LogInPage: React.FC = () => {
   const [password, setPassword] = useState<string>("");
 
   const [login, setLogin] = useRecoilState(loginState);
-  const [user, setUser] = useRecoilState(userState);
+  const [currentUser, setCurrentUser] = useRecoilState(userState);
 
   useEffect(() => {
-    if (user || login) navigate("/");
-  }, [user, login]);
+    if (currentUser || login) navigate("/");
+  }, [currentUser, login]);
 
   const navigate = useNavigate();
 
@@ -32,7 +32,7 @@ const LogInPage: React.FC = () => {
     try {
       e.preventDefault();
       const data = await logInWithEmailAndPassword(email, password);
-      setUser(data?.user?.uid);
+      setCurrentUser(data?.user?.uid);
       navigate("/bookshelf");
     } catch (err) {
       // console.log(err);
@@ -42,7 +42,7 @@ const LogInPage: React.FC = () => {
   const handleGoogleLogin = async () => {
     try {
       const data = await signInWithGoogle();
-      setUser(data?.user?.uid);
+      setCurrentUser(data?.user?.uid);
       navigate("/bookshelf");
     } catch (err) {
       // console.error(err);
@@ -52,6 +52,16 @@ const LogInPage: React.FC = () => {
   return (
     <>
       <h1>로그인</h1>
+      {/* {currentUser && (
+        <div className="flex gap-4 items-start text-start mx-10 mb-5 border border-1 border-slate-700 px-7 py-5 rounded-lg">
+          <div className="skeleton w-[85px] h-[115px] rounded-lg shrink-0"></div>
+          <div className="flex flex-col gap-3">
+            <div className="skeleton w-120 mt-1 h-5 rounded-sm" />
+            <div className="skeleton w-20 h-4" />
+            <div className="skeleton w-32 h-6 rounded-lg" />
+          </div>
+        </div>
+      )} */}
       <section className="flex flex-col mx-auto max-w-xs my-20 gap-2 ">
         <form onSubmit={handleSubmit} className="flex flex-col gap-2">
           <input
