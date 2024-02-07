126d5b7fa075aeaad7266839a886a6cf803965d0
yerinra <yerinaleph@gmail.com>
2024-02-08 00:00:13 +0900

    💄 Style: 홈페이지 스타일링

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,38 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import "./App.css";
 
 import RoutesPage from "./RoutesPage";
-
-import { useRecoilState } from "recoil";
-import { loginState, userState } from "../src/store/userState";
-import { useEffect } from "react";
-import { auth, db } from "./service/firebase";
-import { collection, onSnapshot } from "firebase/firestore";
-import { booksState } from "./store/booksState";
-// import { onAuthStateChanged } from "firebase/auth";
-import { themeState } from "./store/themeState";
+import useThemeEffect from "./hooks/useThemeEffect";
+import useUser from "./hooks/useUser";
 
 function App() {
-  const [login, setLogin] = useRecoilState(loginState);
-  const [currentUser, setCurrentUser] = useRecoilState(userState);
-  const [bookList, setBookList] = useRecoilState(booksState);
-  const [theme, setTheme] = useRecoilState(themeState);
-
   const queryClient = new QueryClient();
+  useThemeEffect();
+  useUser();
 
-  useEffect(() => {
-    const getCurrentUser = () => {
-      auth.onAuthStateChanged((user) => {
-        if (user) {
-          setLogin(true);
-          setCurrentUser(user?.uid);
-        } else {
-          setLogin(false);
-        }
-      });
-    };
-
-    getCurrentUser();
-  }, []);
-
-  useEffect(() => {
-    const getBookList = async () => {
-      if (currentUser) {
-        try {
-          onSnapshot(collection(db, "users", currentUser, "books"), (doc) => {
-            const mapped = doc?.docs?.map((doc) => doc?.data());
-            setBookList(mapped);
-          });
-        } catch (e) {
-          console.error(e);
-        }
-      }
-    };
-    getBookList();
-  }, []);
-
-  useEffect(() => {
-    if (
-      window.matchMedia("(prefers-color-scheme: dark)").matches ||
-      localStorage.getItem("theme") == "dracula"
-    ) {
-      document.documentElement.setAttribute("data-theme", "dracula");
-      setTheme("dracula");
-    } else {
-      document.documentElement.setAttribute("data-theme", "emerald");
-      localStorage.setItem("theme", "emerald");
-      setTheme("emerald");
-    }
-  }, []);
+  // useEffect(() => {
+  //   const getBookList = async () => {
+  //     if (currentUser) {
+  //       try {
+  //         onSnapshot(collection(db, "users", currentUser, "books"), (doc) => {
+  //           const mapped = doc?.docs?.map((doc) => doc?.data());
+  //           setBookList(mapped);
+  //         });
+  //       } catch (e) {
+  //         console.error(e);
+  //       }
+  //     }
+  //   };
+  //   getBookList();
+  // }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
       <Router>
         <RoutesPage />
       </Router>
-      <ReactQueryDevtools />
+      {/* <ReactQueryDevtools /> */}
     </QueryClientProvider>
   );
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,20 +6,17 @@ const HomePage = () => {
   const currentUser = useRecoilValue(userState);
   const navigate = useNavigate();
   return (
-    <div className="mx-7 h-screen flex flex-col justify-center items-center gap-16">
-      <p className="text-5xl font-extrabold">
-        <span className="bg-primary px-2 leading-snug cursor-none">
-          나만의 책장
-        </span>
-        을 만들어 보세요.
+    <div className="flex flex-col h-100 justify-center items-center gap-16">
+      <p className="text-5xl font-extrabold mt-44">
+        <span className="text-accent">나만의 책장</span>을 만들어 보세요.
       </p>
-      <p className="line-clamp-2 leading-6 px-5">
+      <p className="line-clamp-2 leading-6 px-5 ">
         읽은 책을 빠짐없이 기록하고, 직접 만든 책장에 추가해서 더 풍요로운
         독서생활을 즐겨보세요 🎉
       </p>
       <section className="flex flex-col sm:flex-row gap-4">
         <button
-          className="btn btn-outline btn-primary text-base-100 px-7"
+          className="btn btn-home"
           onClick={() => {
             navigate("/info");
           }}
@@ -27,7 +24,7 @@ const HomePage = () => {
           무엇을 할 수 있나요?
         </button>
         <button
-          className="btn btn-primary px-12"
+          className="btn btn-home"
           onClick={() => {
             if (currentUser) navigate("/bookshelf");
             else navigate("/login");
