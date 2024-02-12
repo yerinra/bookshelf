1de449fa687891bb13f620cae4d9821dfb478bb8
yerinra <yerinaleph@gmail.com>
2024-02-12 20:47:26 +0900

    ✨ Feat: zod로 유효성 검사 기능 구현

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,4 +1,5 @@
 import { Timestamp } from "firebase/firestore";
+import { z } from "zod";
 
 export type Theme = "light" | "dark";
 
@@ -13,3 +14,28 @@ export type Book = {
   title: string;
   createdAt: Timestamp;
 };
+
+export type BookInfo = {
+  author: string;
+  title: string;
+  cover: string;
+  description: string;
+  isbn13: string;
+  pubDate: Date;
+  itemPage: number;
+};
+
+export type SortOptions = "createdAt" | "title" | "author" | "rating";
+
+export const signUpSchema = z
+  .object({
+    email: z.string().email(),
+    password: z.string().min(8, "비밀번호는 최소한 8글자여야 합니다."),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "입력한 비밀번호와 다릅니다.",
+    path: ["confirmPassword"],
+  });
+
+export type TSignUpSchema = z.infer<typeof signUpSchema>;
--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,23 +1,22 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { signUpWithEmailAndPassword } from "../service/auth";
 import { useRecoilValue } from "recoil";
 import { loginState, userState } from "../store/userState";
 import { db } from "../service/firebase";
 import { doc, setDoc } from "firebase/firestore";
-import { useForm, FieldValues } from "react-hook-form";
+import { useForm } from "react-hook-form";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { TSignUpSchema, signUpSchema } from "../lib/types";
 
 export default function SignUpPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
   const {
     register,
     handleSubmit,
     formState: { errors, isSubmitting },
     reset,
-    getValues,
-  } = useForm();
+  } = useForm<TSignUpSchema>({ resolver: zodResolver(signUpSchema) });
 
   const login = useRecoilValue(loginState);
   const user = useRecoilValue(userState);
@@ -28,18 +27,7 @@ export default function SignUpPage() {
     if (login || user) navigate("/");
   }, [login, user, navigate]);
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.name === "email") setEmail(e.target.value);
-    else if (e.target.name === "password") setPassword(e.target.value);
-  };
-
-  const handleClick = (e: React.MouseEvent<HTMLInputElement>) => {
-    const { name } = e.target as HTMLInputElement;
-    if (name === "email") setEmail("");
-    else if (name === "password") setPassword("");
-  };
-
-  const onSubmit = async (data: FieldValues) => {
+  const onSubmit = async (data: TSignUpSchema) => {
     try {
       const userData = await signUpWithEmailAndPassword(
         data.email,
@@ -65,43 +53,30 @@ export default function SignUpPage() {
 
       <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2">
         {errors.email && (
-          <p className="text-red-500">{`${errors.email.message}`}</p>
+          <p className="errorMessage">{`${errors.email.message}`}</p>
         )}
         {errors.password && (
-          <p className="text-red-500">{`${errors.password.message}`}</p>
+          <p className="errorMessage">{`${errors.password.message}`}</p>
         )}
         {errors.confirmPassword && (
-          <p className="text-red-500">{`${errors.confirmPassword.message}`}</p>
+          <p className="errorMessage">{`${errors.confirmPassword.message}`}</p>
         )}
         <input
-          {...register("email", { required: "Email is required" })}
+          {...register("email")}
           type="email"
           placeholder="Email"
-          onClick={handleClick}
           className="input w-[320px] pl-3 py-2 rounded-md"
         />
         <input
-          {...register("password", {
-            required: "Password is required",
-            minLength: {
-              value: 10,
-              message: "Password must be at least 10 characters",
-            },
-          })}
+          {...register("password")}
           type="password"
           placeholder="Password"
-          onClick={handleClick}
           className="input w-[320px] pl-3 py-2 rounded-md"
         />
         <input
-          {...register("confirmPassword", {
-            required: "Please confirm the password",
-            validate: (value) =>
-              value === getValues("password") || "Passwords must match.",
-          })}
+          {...register("confirmPassword")}
           type="password"
           placeholder="Confirm Password"
-          onClick={handleClick}
           className="input w-[320px] pl-3 py-2 rounded-md"
         />
         <button
