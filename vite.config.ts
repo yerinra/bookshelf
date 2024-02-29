08455d0e5f38e18822fef154a209b4515d7ee72b
yerinra <yerinaleph@gmail.com>
2024-02-29 16:53:10 +0900

    ⚡️ Chore: minify js

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig, type PluginOption } from "vite";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
+import terser from "@rollup/plugin-terser";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-
+  plugins: [react(), visualizer() as PluginOption],
+  build: {
+    minify: "terser", // Use terser for minification
+    rollupOptions: {
+      plugins: [terser()],
+    },
+  },
   server: {
     proxy: {
       "/keyword": {
